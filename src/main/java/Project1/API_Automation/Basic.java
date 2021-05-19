e29d752423deb48bb9436bfbaf39593e977bb445
krishnachaitanyabitra <krishna.bitra924@gmail.com>
2021-05-19 21:43:35 +0530

    commit for second project

---
+++ src/main/java/Project1/API_Automation/Basic.java
@@ -0,0 +1,14 @@
+package Project1.API_Automation;
+
+
+
+
+public class Basic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	RestAssured
+	}
+
+}
---
+++ src/test/java/Project1/API_Automation/AppTest.java
@@ -0,0 +1,20 @@
+package Project1.API_Automation;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
